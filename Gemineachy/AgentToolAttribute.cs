482610e0d1db875c2b714f231a6e60b7ef36e65a
Todd Tanner <todd@spawndev.com>
2026-08-09 19:14:47 -0400

    Tooling

--- Gemineachy/AgentToolAttribute.cs
+++ Gemineachy/AgentToolAttribute.cs
@@ -1,8 +1,14 @@
 ﻿namespace Gemineachy
 {
+    /// <summary>
+    /// Used to mark a method as am agent tool
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
     public class AgentToolAttribute : Attribute
     {
+        /// <summary>
+        /// Tool description
+        /// </summary>
         public string Description { get; private set; }
         public AgentToolAttribute(string description)
         {
